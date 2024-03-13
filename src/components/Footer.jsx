7c0f4e59670b7ed937f80a490fe91aa37a0f78e0
Rafael Martins <127500319+deoliverrafa@users.noreply.github.com>
2024-03-13 10:10:20 -0300

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="rodape bg-slate-700">
-            <p className="text-white font-bold ">Desenvolvido por Rafael</p>
-        </footer>
-    )
-}
